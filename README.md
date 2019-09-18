365822b94f4ab5c5b055d2332249ec08731951d4
Yuliya Kharchenko <y.harchenko@propellerads.net>
2019-09-18 14:02:34 +0300

    Bastion and VPN

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Lisskha_infra
 Lisskha Infra repository
+
+bastion_IP = 34.90.72.45
+someinternalhost_IP = 10.164.0.5
---
+++ setupvpn.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+tee /etc/yum.repos.d/mongodb-org-3.4.repo << EOF
+[mongodb-org-3.4]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/7/mongodb-org/3.4/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-3.4.asc
+EOF
+
+tee /etc/yum.repos.d/pritunl.repo << EOF
+[pritunl]
+name=Pritunl Repository
+baseurl=https://repo.pritunl.com/stable/yum/centos/7/
+gpgcheck=1
+enabled=1
+EOF
+
+rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+gpg --armor --export 7568D9BB55FF9E5287D586017AE645C0CF8E292A > key.tmp; sudo rpm --import key.tmp; rm -f key.tmp
+yum -y install pritunl mongodb-org
+systemctl start mongod pritunl
+systemctl enable mongod pritunl
